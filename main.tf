6c882856a3f4ae829c5aba71a09dd26cb302f73d
sriram1469 <burasriram2002@gmail.com>
2024-10-08 12:15:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "web" {
   ami           = "ami-0ebfd941bbafe70c6"
-  instance_type = var.instance_type
-  count=3
+  instance_type = "t2.micro"
+ 
 
   tags = {
-    Name = var.countname [count.index]
-    module = "x"
+    Name = "sriram"
+    
   }
 }
